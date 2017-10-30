74457e99f4c3e22b248a72291d0ca946068f29c2
3ddy10000 <eivind@hukkelberg.net>
2017-10-30 18:01:21 +0100

    endringer og forbedringer

--- Staging/Eivind/script.js
+++ Staging/Eivind/script.js
@@ -4,7 +4,21 @@ window.onload = function() {
 	setInterval(run, 1000/30);
 }
 
+var x = 0;
+var h = 25;
+
 var run = function() {
-	ctx.fillStyle = "#FFF";
+
+	if (x + h > canv.height) {
+		x = 0;
+		h += 25;
+	}
+
+	ctx.fillStyle = "#333";
 	ctx.fillRect(0, 0, canv.width, canv.height);
+
+	ctx.fillStyle = "#222";
+	ctx.fillRect(0, x, canv.width, h);
+
+	x++;
 }
