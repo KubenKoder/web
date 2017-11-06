19edc674ffbc0ff737afa347a87809ec2b26b334
3ddy10000 <eivind@hukkelberg.net>
2017-11-06 16:59:14 +0100

    La til snake på nettsiden

--- Staging/Eivind/script.js
+++ Staging/Eivind/script.js
@@ -4,21 +4,6 @@ window.onload = function() {
 	setInterval(run, 1000/30);
 }
 
-var x = 0;
-var h = 25;
-
 var run = function() {
 
-	if (x + h > canv.height) {
-		x = 0;
-		h += 25;
-	}
-
-	ctx.fillStyle = "#333";
-	ctx.fillRect(0, 0, canv.width, canv.height);
-
-	ctx.fillStyle = "#222";
-	ctx.fillRect(0, x, canv.width, h);
-
-	x++;
 }
